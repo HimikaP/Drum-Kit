f95e49bd6ff861b98b34e3a80bf54dcc3398c6ed
HimikaP <prabhat.himika@gmail.com>
2022-10-12 14:54:43 +0530

    about to add sounds

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var i=1
-while(i<document.querySelectorAll(".drum").length){
-    document.querySelectorAll("button")[i].addEventListener("click", function (){
-        alert("Oh ! I got clicked");
-    });
+// var i=1
+// while(i<document.querySelectorAll(".drum").length){
+//     document.querySelectorAll("button")[i].addEventListener("click", function (){
+//         alert("Oh ! I got clicked");
+//     });
     
-    i++;
-}
+//     i++;
+// }
 
