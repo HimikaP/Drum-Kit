2e0f4565ac8d6bfe2c82953de710c6598b1f6296
HimikaP <prabhat.himika@gmail.com>
2022-10-12 16:27:22 +0530

    another commit

--- index.js
+++ index.js
@@ -2,7 +2,37 @@ var i=1
 while(i<document.querySelectorAll(".drum").length){
     document.querySelectorAll("button")[i].addEventListener("click", function (){
         // this.style.color = 'white';
+
+        var alpha = this.innerHTML;
+        console.log(alpha)
+        
        
+        switch(alpha) {
+            case "w" : var sound = new Audio('sounds/crash.mp3');
+                        sound.play();
+                        break;
+            case "a" : var sound = new Audio('sounds/tom-2.mp3');
+                        sound.play();
+                        break;
+            case "s" : var sound = new Audio('sounds/tom-3.mp3');
+                        sound.play();
+                        break;
+            case "d" : var sound = new Audio('sounds/tom-4.mp3');
+                        sound.play();
+                        break;
+            case "j" : var sound = new Audio('sounds/crash.mp3');
+                        sound.play();
+                        break;
+            case "k" : var sound = new Audio('sounds/kick.mp3');
+                        sound.play();
+                        break;
+            case "l" : var sound = new Audio('sounds/snare.mp3');
+                        sound.play();
+                        break;
+
+                        default: console.log("erroneous code");
+
+        }
     
     });
     
